04841bc75ce82c1fc5de221cd6181a5827499f41
Shohei Kameda <shoheik@cpan.org>
2013-05-06 11:08:30 +0900

    added many things on homenotes.js

--- public/js/homenotes.js
+++ public/js/homenotes.js
@@ -9,7 +9,7 @@
     // };
 
     //=========================================================================
-    // To submit knowhow 
+    // Submit knowhow 
     //=========================================================================
     // knowhow model for submittion
     var Knowhow = Backbone.Model.extend({
@@ -89,8 +89,8 @@
                             alert("Failed to insert. Please contact admin");
                         }else {
                             alert("Thank you for sharing with us!");
-                            location.href = "/knowhow/view/" + response;
-                            //router.navigate("/knowhow/view/" + response);
+                            router.navigate("/knowhow/view/" + response, {trigger: true});
+                            $('#myModal').modal('hide');
                         }
                     }
                 }
@@ -110,20 +110,116 @@
     $('#myModal').append(modalfooter.render().el);
 
     //=========================================================================
-    // To show knowhow 
+    // Show knowhow 
     //=========================================================================
     var KnowhowResult = Backbone.Model.extend({
         urlRoot: '/knowhow'
     });
     var KnowhowResultView = Backbone.View.extend({
-        template: _.template("<h2>KNOW</h2><pre><code><%- know %></code></pre><h2>HOW</h2><h2>EXAMPLE</h2>"),
+        template: _.template("<h2>KNOW</h2><pre><code><%- know %></code></pre>" 
+            + "<h2>HOW</h2><pre><code><%- how %></code></pre>" 
+            + "<h2>EXAMPLE</h2><pre><code><%- example %></code></pre>"),
         render: function(){
             var html = this.template(this.model.toJSON());
-            console.log(html);
             $('#view').html(html);
         }
     });
-    
+
+    // Used by Router
+    var showKnowhow = function(id) {
+        var knowhow = new KnowhowResult({id : id});
+        knowhow.fetch({
+            success: function(res) {
+                var knowhowview = new KnowhowResultView({model: knowhow});
+                knowhowview.render();
+            }
+        });
+    }
+
+    //=========================================================================
+    // Search
+    //=========================================================================
+
+    var SearchText = Backbone.View.extend({
+        el: $('#search_text'),
+    });
+
+    var SearchView = Backbone.View.extend({
+        el: $('#search'),
+        initialize: function(){
+            _.bindAll(this, 'click_search');
+            _.bindAll(this, 'render');
+        },
+        events: {
+            'click' : 'click_search',
+        },
+        click_search: function(e) { 
+            var search = {}
+            search.query = $('#search_text').val();
+            search.know = 0;
+            search.how = 0;
+            search.example = 0;
+            $("input[name=check]:checked").map(function(){
+                var v = $(this).val();
+                if (v == 'know') {
+                    search.know = 1;
+                }else if ( v == 'how' ) {
+                    search.how = 1;
+                }else if ( v == 'example' ) {
+                    search.example = 1;
+                }
+            });
+            var self = this;
+            this.model.fetch({
+                data : search,
+                success: function(model, res){
+                    self.model.set({result: res });
+                    self.render();
+                    router.navigate("result");
+                }
+            });
+        },
+        render: function(){
+            var result ="";
+            var res = this.model.get("result");
+            console.log(res);
+            result += '<div class="span3"></div><div class="span6">';
+            if (res === "" ){
+                result += "<center><strong>No Search Result</strong></center><br><br><br>";
+            }else {
+                for (var i in res ){
+                  result += '<a class="searchresult" href="/knowhow/view/' + i + '" >' + res[i] + '</a><hr>';
+                }
+            }
+            result += '</div><div class="span3"></div>';
+            $("#page_info").html('');
+            $("#view").html(result);
+        }
+
+    });
+
+    var SearchQuery = Backbone.Model.extend({
+        url: '/search',
+    });
+    var query = new SearchQuery();
+    var searchview = new SearchView({model: query});
+    var searchtext = new SearchText(
+            {
+                events: { 
+                    'keypress': function(e) { 
+                        if(e.keyCode == 13){
+                            searchview.click_search();
+                        } 
+                    }
+                }
+            }
+    ); 
+
+    $(document).on('click', '.searchresult', function(e){
+        e.preventDefault();
+        Backbone.history.navigate($(this).attr('href'), {trigger: true});
+    });
+
     
     //=========================================================================
     // Router 
@@ -132,22 +228,16 @@
         routes: {
             "knowhow/view/:id": "knowhow",
             "": 'top',
+            "result": "result",
         },
-        "knowhow": function(id){
-            alert(id);
-            var knowhow = new KnowhowResult({id : id});
-            knowhow.fetch({
-                success: function(res) {
-                    console.log(knowhow.toJSON());
-                    var knowhowview = new KnowhowResultView({model: knowhow});
-                    knowhowview.render();
-                }
-            });
-
-        },
-        'top': function(id){
+        "knowhow": showKnowhow,
+        'top': function(){
             console.log('top');
-        }
+            location.href = '/index'; 
+        },
+        'result': function() { searchview.render() }
+        
+        
     }); 
     var router = new HomenotesRouter();
     Backbone.history.start({ pushState: true });
